d104eef4f39f582adad81260d2794fd053d5cb63
Emran BatmanGhelich <batmanghelich@inside.sahab.ir>
2019-10-23 10:32:54 +0330

    Other: Deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+then
+>&2 echo "Usage: $0 <ftp host> <username> <password>"
+exit 1
+fi
+
+URL=$1
+USERNAME=$2
+PASS=$3
+
+which lftp > /dev/null
+if [ $? -ne 0 ]
+then
+>&2 echo "'lftp' is not installed."
+exit 2
+fi
+
+set -e
+
+lftp -c "set ftp:ssl-allow no; open -u $USERNAME,$PASS $URL; mirror -Rev public_html/ .  --ignore-time --parallel=10"
