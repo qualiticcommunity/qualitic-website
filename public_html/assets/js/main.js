35ce9d1b4248a1c2f66b01b691e9b61268a51971
Emran Batmanghelich <emran.bm@gmail.com>
2022-01-17 18:39:19 +0330

    Remove event timer and "Add to calendar"

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -17,41 +17,6 @@
         }
     });
 
-    /* ==========================================================================
-       countdown timer
-       ========================================================================== */
-
-    function toFa(enText) {
-      const enToFaNumberMap = {
-          "0": "۰",
-          "1": "۱",
-          "2": "۲",
-          "3": "۳",
-          "4": "۴",
-          "5": "۵",
-          "6": "۶",
-          "7": "۷",
-          "8": "۸",
-          "9": "۹"
-      };
-
-      return enText !== undefined
-          ? enText
-                .toString()
-                .split("")
-                .map(enChar => enToFaNumberMap[enChar] || enChar)
-                .join("")
-          : "";
-    };
-
-     jQuery('#clock').countdown('2019/11/12 17:00',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry seconds"><span>' + toFa(event.offset.seconds) + '</span><strong>ثانیه</strong></div> '
-	  +'<div class="time-entry minutes"><span>' + toFa(event.offset.minutes) + '</span><strong>دقیقه</strong></div>'
-      +'<div class="time-entry hours"><span>' + toFa(event.offset.hours) + '</span><strong>ساعت</strong></div> '
-      +'<div class="time-entry days"><span>' + toFa(event.offset.totalDays) + '</span><strong>روز</strong></div> '));
-    });
-
     /* slicknav mobile menu active  */
     $('.mobile-menu').slicknav({
         prependTo: '.navbar-header',
